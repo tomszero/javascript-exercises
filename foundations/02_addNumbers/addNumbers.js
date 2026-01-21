8d8987e62fdf15363854d7232200cef42324eea4
tomszero <tomasm.saez@gmail.com>
2026-01-21 16:51:12 -0400

    Solves exercises 1 through 4

--- foundations/02_addNumbers/addNumbers.js
+++ foundations/02_addNumbers/addNumbers.js
@@ -4,7 +4,7 @@ function addNumbers() {
 
   let result;
 
-  result = "a" + "b"; // <------ EDIT THIS LINE
+  result = a + b; // This line adds the two numbers
 
   return result;
 }
--- foundations/04_mathEquations/mathEquations.js
+++ foundations/04_mathEquations/mathEquations.js
@@ -1,9 +1,9 @@
 const a = 1 - 1 // Freebie!!! This is the answer to "one minus one"
-const b = "one plus eight"
-const c = "22 times three"
-const d = "the *remainder* of 5/4"
-const e = "the variable 'b' minus 17"
-const f = "the sum of the previous five variables"
+const b = 1 + 8 // This is the answer to "one plus eight"
+const c = 22 * 3 // This is the answer to "twenty-two times three"
+const d = 5 % 4 // This is the answer to "the remainder of five divided by four"
+const e = b - 17 // This is the answer to "b minus seventeen"
+const f = a + b + c + d + e // This is the answer to "a plus b plus c plus d plus e"
 
 // Do not edit below this line
 module.exports = {a, b, c, d, e, f}
